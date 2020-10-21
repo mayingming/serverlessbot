2d533ae32504a9be3d7c26e8a47073587bf73fc5
mayingming <mayingming1202@gmail.com>
2020-10-21 21:43:49 +1100

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ ACCESS_TOKEN =
 ACCESS_TOKEN_SECRET = 
 TWITTER_NAME = "Your twitter user name"
 # Post a twitter in every set minute
-TIME_SLOT = 1
 MESSAGE_FILE = "test.csv"
 TEMP_FILE = "temp.csv"
 TEMP_FILE_TO_DELETE = "todelete.csv"
